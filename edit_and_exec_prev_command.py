0fa31d15f0e174caa4bd24a4f0e248aaf9bb8cb9
Liviu Popa <liviucst@gmail.com>
2019-11-06 10:33:36 +0000

    Converted execCommandBasedOnPrevInput() to Python
    This was required as part of the BASH to Python script migration process

---
+++ edit_and_exec_prev_command.py
@@ -0,0 +1,35 @@
+import sys, os, readline
+import execute_command as cmd
+from os.path import expanduser
+
+home_dir = expanduser("~") + "/"
+input_storage_file = home_dir + ".store_input"
+
+def edit_and_exec():
+    if len(sys.argv) == 1:
+        print("No shell command previously executed. Enter a new command")
+    else:
+        print("Please edit the below command and hit ENTER to execute")
+        readline.set_pre_input_hook(hook)
+
+    print("(press \':\' + ENTER to quit):")
+    commandToExecute = input()
+    readline.set_pre_input_hook() # ensure any further input is no longer pre-filled
+
+    os.system("clear")
+    if commandToExecute == "" or commandToExecute[len(commandToExecute)-1] == ':':
+        print("Command aborted. You returned to navigation menu.")
+        sys.exit(1)
+    else:
+        # forward input command to BASH for further usage
+        with open(input_storage_file, "w") as input_storage:
+            input_storage.write(commandToExecute)
+        cmd.execute(commandToExecute)
+        sys.exit(0)
+
+def hook():
+    prevCommand = sys.argv[1]
+    readline.insert_text(prevCommand)
+    readline.redisplay()
+
+edit_and_exec()
--- execute_new_command.py
+++ execute_new_command.py
@@ -1,61 +1,15 @@
-import sys, os, datetime
+import sys, os
 from os.path import expanduser
-from consolidate_command_history import consolidate
+import execute_command as cmd
 
 home_dir = expanduser("~") + "/"
 c_r_hist_file = home_dir + ".recent_command_history"
 output_storage_file = home_dir + ".store_output"
 
 def executeNew():
-    minNrOfCmdChars = 10
     if len(sys.argv) == 1:
         print("No argument provided")
     else:
-        commandToExecute = sys.argv[1]
-        if len(commandToExecute) >= minNrOfCmdChars:
-            updateIndividualCommandHistoryFiles(commandToExecute)
-            consolidate()
-
-        print("Command is being executed: " + commandToExecute)
-        print("--------------------------")
-
-        # build and execute command
-        sourceCommand = "source ~/.bashrc;" #include .bashrc to ensure the aliases and scripts work
-        executionStatus = "echo $? > " + output_storage_file
-        executeCommandWithStatus = sourceCommand + "\n" + commandToExecute + "\n" + executionStatus
-        os.system(executeCommandWithStatus)
-
-        # read command status code and create the status message
-        with open(output_storage_file, "r") as output:
-            status = output.readline().strip('\n')
-            printedStatus = "with errors" if status != "0" else "successfully"
-
-        # status message to be written instead of code so it's used by BASH to indicate how the last executed command finished (to be updated)
-        with open(output_storage_file, "w") as output:
-            output.write(printedStatus)
-
-        print("--------------------------")
-        print("Command finished " + printedStatus + "! Scroll up to check output (if any) if it exceeds the screen.")
-
-def updateIndividualCommandHistoryFiles(command):
-    c_r_hist_max_entries = 25
-
-    with open(c_r_hist_file, "r") as c_r_hist:
-        c_r_hist_content = []
-        c_r_hist_entries = 0
-        for entry in c_r_hist.readlines():
-            c_r_hist_content.append(entry.strip('\n'))
-            c_r_hist_entries = c_r_hist_entries + 1
-
-    if command in c_r_hist_content:
-        c_r_hist_content.remove(command)
-    elif c_r_hist_entries == c_r_hist_max_entries:
-        c_r_hist_content.remove(c_r_hist_content[c_r_hist_entries-1])
-    c_r_hist_content = [command] + c_r_hist_content
-
-    with open(c_r_hist_file, "w") as c_r_hist:
-        for entry in c_r_hist_content:
-            c_r_hist.write(entry+'\n')
-
+        cmd.execute(sys.argv[1])
 
 executeNew()
