177bbd20bf403d74517299393a0fd0fbfe8e72c0
Liviu C. Popa <liviucst@gmail.com>
2019-12-02 06:33:46 +0000

    Refactored variables from cmd_menus_update.py
    This was required for consistency

--- cmd_menus_update.py
+++ cmd_menus_update.py
@@ -7,12 +7,12 @@ c_hist_file = home_dir + ".command_history"
 c_r_hist_file = home_dir + ".recent_command_history"
 output_storage_file = home_dir + ".store_output"
 c_r_hist_max_entries = 25
-minNrOfCmdChars = 10
+min_nr_of_cmd_chars = 10
 
 # 1) Initialize command environment
 def initCmdMenus():
-    with open(c_r_hist_file, "a") as c_r_hist:
-        c_r_hist.write("")
+    with open(c_r_hist_file, "a") as crHist:
+        crHist.write("")
     # limit the number of entries from recent command history to the maximum allowed
     common.limitEntriesNr(c_r_hist_file, c_r_hist_max_entries)
     # will have a more important role once persistent command history will be implemented
@@ -39,22 +39,22 @@ def displayFormattedCmdHistContent():
 def executeCommand(commandToExecute):
     # *** helper functions ***
     def updateIndividualCommandHistoryFiles(command):
-        with open(c_r_hist_file, "r") as c_r_hist:
-            c_r_hist_content = []
-            c_r_hist_entries = 0
-            for entry in c_r_hist.readlines():
-                c_r_hist_content.append(entry.strip('\n'))
-                c_r_hist_entries = c_r_hist_entries + 1
-        if command in c_r_hist_content:
-            c_r_hist_content.remove(command)
-        elif c_r_hist_entries == c_r_hist_max_entries:
-            c_r_hist_content.remove(c_r_hist_content[c_r_hist_entries-1])
-        c_r_hist_content = [command] + c_r_hist_content
-        with open(c_r_hist_file, "w") as c_r_hist:
-            for entry in c_r_hist_content:
-                c_r_hist.write(entry+'\n')
+        with open(c_r_hist_file, "r") as crHist:
+            crHistContent = []
+            crHistEntries = 0
+            for entry in crHist.readlines():
+                crHistContent.append(entry.strip('\n'))
+                crHistEntries = crHistEntries + 1
+        if command in crHistContent:
+            crHistContent.remove(command)
+        elif crHistEntries == c_r_hist_max_entries:
+            crHistContent.remove(crHistContent[crHistEntries-1])
+        crHistContent = [command] + crHistContent
+        with open(c_r_hist_file, "w") as crHist:
+            for entry in crHistContent:
+                crHist.write(entry+'\n')
     # *** actual function ***
-    if len(commandToExecute) >= minNrOfCmdChars:
+    if len(commandToExecute) >= min_nr_of_cmd_chars:
         updateIndividualCommandHistoryFiles(commandToExecute)
         consolidateCommandHistory()
     # build and execute command
@@ -71,17 +71,17 @@ def executeCommand(commandToExecute):
 
 # 4) Clear command history
 def clearCommandHistory():
-    with open(c_r_hist_file, "w") as c_r_hist:
-        c_r_hist.write("")
-    with open(c_hist_file, "w") as c_hist:
-        c_hist.write("")
+    with open(c_r_hist_file, "w") as crHist:
+        crHist.write("")
+    with open(c_hist_file, "w") as cHist:
+        cHist.write("")
 
 # 5) Shared functions
 def consolidateCommandHistory():
-    with open(c_r_hist_file, 'r') as c_r_hist:
-        c_r_hist_entries = c_r_hist.readlines()
-        c_r_hist_entries.sort()
+    with open(c_r_hist_file, 'r') as crHist:
+        crHistEntries = crHist.readlines()
+        crHistEntries.sort()
     # always ensure the file is cleared before (re-)consolidating history
-    with open(c_hist_file, 'w') as c_hist:
-        for entry in c_r_hist_entries:
-            c_hist.write(entry)
+    with open(c_hist_file, 'w') as cHist:
+        for entry in crHistEntries:
+            cHist.write(entry)
