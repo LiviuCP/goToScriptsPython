6337646caeac936d468b716c8752fc3d3efdc0d4
Liviu C. Popa <liviucst@gmail.com>
2019-12-02 06:56:42 +0000

    Refactored variables from nav_shared.py

--- nav_shared.py
+++ nav_shared.py
@@ -2,28 +2,28 @@ import os
 
 # functions shared within nav_menu_update.py and not used elsewhere
 
-def sortFavorites(fav_file): # to do: handle empty argument
-    fav_dict = {}
-    with open(fav_file, "r") as fav:
-        fav_file_content = fav.readlines()
-        for entry in fav_file_content:
+def sortFavorites(favFile): # to do: handle empty argument
+    favDict = {}
+    with open(favFile, "r") as fav:
+        favFileContent = fav.readlines()
+        for entry in favFileContent:
             entry = entry.strip('\n')
-            fav_dict[entry] = os.path.basename(entry)
-    with open(fav_file, "w") as fav:
-        for entry in sorted(fav_dict.items(), key = lambda k:(k[1].lower(), k[0])):
+            favDict[entry] = os.path.basename(entry)
+    with open(favFile, "w") as fav:
+        for entry in sorted(favDict.items(), key = lambda k:(k[1].lower(), k[0])):
             fav.write(entry[0] + '\n')
 
-def removePathFromTempHistoryFile(hist_file, path): # to do: handle empty arguments
-    item_contained_in_hist_file = False
-    hist_content = []
-    with open(hist_file, "r") as hist:
+def removePathFromTempHistoryFile(histFile, path): # to do: handle empty arguments
+    itemContainedInHistFile = False
+    histContent = []
+    with open(histFile, "r") as hist:
         for entry in hist.readlines():
             if entry.strip('\n') == path:
-                item_contained_in_hist_file = True
+                itemContainedInHistFile = True
             else:
-                hist_content.append(entry)
-    if item_contained_in_hist_file == True:
-        with open(hist_file, "w") as hist:
-            for entry in hist_content:
+                histContent.append(entry)
+    if itemContainedInHistFile == True:
+        with open(histFile, "w") as hist:
+            for entry in histContent:
                 hist.write(entry)
-    return item_contained_in_hist_file
+    return itemContainedInHistFile
