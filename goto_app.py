8d29e08a26e321ebdb79f8ee9f5aa3a1287363d6
Liviu C. Popa <liviucst@gmail.com>
2020-01-23 11:17:17 +0000

    Refactored the handleUserInput() function

--- goto_app.py
+++ goto_app.py
@@ -39,87 +39,80 @@ def handleUserInput(userInput, prevDir, prevCommand):
     handleOutput = 0
     passedInput = ""
     passedOutput = ""
-    shouldForwardData = True
+    shouldForwardData = False
     if userInput == "?":
         out.displayHelp()
-        shouldForwardData = False
     elif userInput == ":-":
         if prevCommand == "":
             print("No shell command previously executed")
-            shouldForwardData = False
         else:
             result = cmd.executeCommand(prevCommand, True)
+            shouldForwardData = True
     elif userInput == ":":
         result = cmd.editAndExecPrevCmd(prevCommand) if prevCommand != "" else cmd.editAndExecPrevCmd()
         handleOutput = 2 if result[0] == 0 else handleOutput
+        shouldForwardData = True
     elif userInput == ":<":
         result = cmd.visitCommandMenu("--execute")
         handleOutput = 2 if result[0] == 0 else 1 if result[0] == 1 else handleOutput
+        shouldForwardData = True
     elif userInput == "::":
         result = cmd.visitCommandMenu("--edit")
         handleOutput = 2 if result[0] == 0 else 1 if result[0] == 1 else handleOutput
+        shouldForwardData = True
     elif userInput == "::<>":
         cmd.clearCommandHistory()
-        shouldForwardData = False
     elif userInput == "<":
         result = nav.executeGoToFromMenu("-h", prevDir)
         handleOutput = 4 if result[0] == 0 else 1 if result[0] == 1 else handleOutput
+        shouldForwardData = True
     elif userInput == ">":
         result = nav.executeGoToFromMenu("-f", prevDir)
         handleOutput = 4 if result[0] == 0 else 1 if result[0] == 1 else handleOutput
+        shouldForwardData = True
     elif len(userInput) > 1 and userInput[0] == "<":
         result = nav.executeGoToFromMenu("-h", prevDir, userInput[1:])
         handleOutput = 4 if result[0] == 0 else 1 if (result[0] == 1 or result[0] == 4) else handleOutput #forward user input if history menu is empty and the user enters <[entry_nr] (result == 4)
+        shouldForwardData = True
     elif len(userInput) > 1 and userInput[0] == ">":
         result = nav.executeGoToFromMenu("-f", prevDir, userInput[1:])
         handleOutput = 4 if result[0] == 0 else 1 if (result[0] == 1 or result[0] == 4) else handleOutput #forward user input if favorites menu is empty and the user enters >[entry_nr] (result == 4)
+        shouldForwardData = True
     elif userInput == ",":
         result = nav.goTo(prevDir, os.getcwd())
         handleOutput = 4 if result[0] == 0 else handleOutput
+        shouldForwardData = True
     elif userInput == "+>":
         nav.addDirToFavorites()
-        shouldForwardData = False
     elif userInput == "->":
         result = nav.removeDirFromFavorites()
         handleOutput = 1 if result[0] == 1 else handleOutput
+        shouldForwardData = True
     elif userInput == ":<>":
         nav.clearVisitedDirsMenu()
-        shouldForwardData = False
     elif userInput == ":c":
         clipboard.createAction()
-        shouldForwardData = False
     elif userInput == ":m":
         clipboard.createAction(False)
-        shouldForwardData = False
     elif userInput == ":y":
         clipboard.applyAction()
-        shouldForwardData = False
     elif userInput == ":ec":
         clipboard.erase()
-        shouldForwardData = False
     elif userInput == ":dc":
         clipboard.display()
-        shouldForwardData = False
     elif userInput == ":td":
         clipboard.setTargetDir()
-        shouldForwardData = False
     elif userInput == ":M":
         clipboard.transferItemsToTargetDir("move")
-        shouldForwardData = False
     elif userInput == ":C":
         clipboard.transferItemsToTargetDir("copy")
-        shouldForwardData = False
     elif userInput == ":etd":
         clipboard.eraseTargetDir()
-        shouldForwardData = False
     elif userInput == ":dtd":
         clipboard.displayTargetDir()
-        shouldForwardData = False
     elif len(userInput) > 1 and userInput[len(userInput)-1] == ":":
-        shouldForwardData = False
         print("Input cancelled, no action performed!")
     elif userInput == "!":
-        shouldForwardData = False
         print("You exited navigation app.")
         print("Last visited directory: " + os.getcwd())
     else:
@@ -129,6 +122,7 @@ def handleUserInput(userInput, prevDir, prevCommand):
         else:
             result = nav.goTo(userInput, prevDir)
             handleOutput = 4 if result[0] == 0 else handleOutput
+        shouldForwardData = True
     if shouldForwardData == True:
         passedInput = result[1]
         passedOutput = result[2]
