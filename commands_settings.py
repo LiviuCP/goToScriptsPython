2a018f905ebfea008341baff2acde02d1269de6b
Liviu Constantin POPA <liviucst@gmail.com>
2020-06-06 18:25:55 +0300

    Created a separate commands settings file
    This was required for better code decoupling/encapsulation

---
+++ commands_settings.py
@@ -0,0 +1,13 @@
+import datetime
+from os.path import expanduser
+
+home_dir = expanduser("~") + "/"
+c_hist_file = home_dir + ".command_history"
+c_r_hist_file = home_dir + ".recent_command_history"
+c_p_str_hist_file = home_dir + ".persistent_command_history_strings" # actual commands
+c_p_num_hist_file = home_dir + ".persistent_command_history_numbers" # number of times each command was executed (each row should match a row from the c_p_str_hist_file)
+c_r_hist_max_entries = 10
+c_p_hist_max_entries = 15
+max_filtered_c_hist_entries = 5
+c_log_dir = home_dir + ".goToCmdLogs/"
+c_l_hist_file = c_log_dir + datetime.datetime.now().strftime("%Y%m%d")
