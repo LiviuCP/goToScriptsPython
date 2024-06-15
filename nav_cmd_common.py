b1cdafe3064e1662053288a78587448ccbbccaa3
Liviu Popa <liviucst@gmail.com>
2024-06-15 18:57:42 +0300

    Refactored fct getMenuEntry() from nav_cmd_common
    It was required to make this function a polymorphic class method and split it between the
    common functionality (used by both navigation and commands - NavCmdCommon) and
    navigation-specific functionality (NavigationBackend)

--- nav_cmd_common.py
+++ nav_cmd_common.py
@@ -1,7 +1,6 @@
 """ common code to be used by navigation_backend.py and commands_backend.py """
 
 import os, re, time
-from pathlib import Path
 
 class NavCmdCommon:
     def __init__(self, settings):
@@ -13,10 +12,10 @@ class NavCmdCommon:
         self.settings = settings
 
     def chooseHistoryMenuEntry(self, userInput):
-        return getMenuEntry(userInput, self.consolidatedHistory)
+        return self.__retrieveMenuEntry__(userInput, self.consolidatedHistory)
 
     def chooseFilteredMenuEntry(self, userInput, filteredContent):
-        return getMenuEntry(userInput, filteredContent)
+        return self.__retrieveMenuEntry__(userInput, filteredContent)
 
     def updateHistory(self, entry):
         assert len(entry) > 0, "Empty argument detected"
@@ -132,41 +131,30 @@ class NavCmdCommon:
         if entryInDailyLog:
             self.dailyLog.remove(entry)
         return entryInDailyLog
-"""
-The returned outcome could have following special values in the first field:
-:1 - user input to be forwarded as regular input (path name/command)
-:2 - user exited the command menu, returned to navigation mode
-:3 - invalid first argument
-:4 - empty menu
-"""
-def getMenuEntry(userInput, content):
-    def isInputValid(userInput, content):
-        isValid = True
-        if userInput.isdigit():
-            intInput = int(userInput)
-            if intInput > len(content) or intInput == 0:
-                isValid = False
+    """
+    The returned outcome could have following special values in the first field:
+    :1 - user input to be forwarded as regular input (path name/command)
+    :2 - user exited the command menu, returned to navigation mode
+    :3 - reserved for future use
+    :4 - empty menu
+    """
+    def __retrieveMenuEntry__(self, userInput, content):
+        if isValidMenuEntryNumber(userInput, content):
+            userInput = int(userInput) - 1
+            output = content[userInput].strip("\n")
         else:
+            output = ":4" if len(content) == 0 else ":2" if userInput == '!' else ":1"
+        return (output, userInput, "")
+
+def isValidMenuEntryNumber(userInput, content):
+    isValid = True
+    if userInput.isdigit():
+        intInput = int(userInput)
+        if intInput > len(content) or intInput == 0:
             isValid = False
-        return isValid
-    if isInputValid(userInput, content):
-        userInput = int(userInput) - 1
-        output = content[userInput]
-    # access parent dir of menu entry
-    elif len(userInput) > 1 and userInput[0] == "," and isInputValid(userInput[1:], content):
-        output = str(Path(content[int(userInput[1:])-1].strip("\n")).parent)
-        userInput = ":parent" # used for further differentiation between entry directory and parent in case the returned path is invalid
-    # retrieved path to be used for setting target dir from menu
-    elif len(userInput) > 1 and userInput[0] == "+" and isInputValid(userInput[1:], content):
-        output = str(Path(content[int(userInput[1:])-1].strip("\n")))
-        userInput = ":preceding+" # used for further differentiation between entry directory and parent for setting target dir
-    # retrieved parent path to be used for setting target dir from menu
-    elif len(userInput) > 1 and userInput[0] == "-" and isInputValid(userInput[1:], content):
-        output = str(Path(content[int(userInput[1:])-1].strip("\n")).parent)
-        userInput = ":preceding-" # used for further differentiation between entry directory and parent for setting target dir
     else:
-        output = ":4" if len(content) == 0 else ":2" if userInput == '!' else ":1"
-    return (output.strip("\n"), userInput, "")
+        isValid = False
+    return isValid
 
 def readFromPermHist(strHistFile, numHistFile, histDict):
     with open(strHistFile, "r") as strHist, open(numHistFile, "r") as numHist:
